586e1b61f626945d2e5e5980f31cfda9278ae505
xiaolong70701 <xiaolong70701@gmail.com>
2025-09-27 21:51:50 +0800

    chore: home page UI & font & image

--- liff-frontend/src/pages/Home.jsx
+++ liff-frontend/src/pages/Home.jsx
@@ -1,20 +1,15 @@
-import eatingYogurt from "../assets/images/landing/eating_yougurt.png";
+import eatingYogurt from "../assets/images/landing/eating_yougurt.svg";
 
 function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-base-100 px-6">
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-        <div className="space-y-6">
-          <h1 className="text-4xl md:text-5xl font-bold leading-tight text-gray-900">
+        
+        <div className="space-y-6 text-center md:text-left">
+          <h1 className="text-5xl md:text-7xl leading-tight text-gray-900 font-rampart font-extrabold text-outline">
             品菓優格，<br />
             Ringo Yogurt
           </h1>
-          <p className="text-lg text-gray-600">
-            我們相信天然的食材，有益於你的健康。
-          </p>
-          <p className="text-lg text-gray-600">
-            我們相信單純的優格，能讓你享受美味。
-          </p>
         </div>
 
         <div className="flex items-center justify-center">
